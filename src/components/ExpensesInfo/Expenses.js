22708d639a77e99f7b9204fe79f0b81255cd283e
王柏茂 <maurice0116@hotmail.com>
2021-09-04 13:19:13 +0800

    Working with Lits (Filtered) practice

--- src/components/ExpensesInfo/Expenses.js
+++ src/components/ExpensesInfo/Expenses.js
@@ -6,18 +6,25 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
-    /* */
+    let outputExpenses = props.expenseData;
+
     const [filteredYear, setFilteredYear] = useState('2020');
 
     const changeExpensesFilterHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     }
 
+    const createOutputExpenses = (filteredYear) => {
+        return props.expenseData.filter((expense) => expense.date.getFullYear().toString() === filteredYear);
+    }
+
+    outputExpenses = createOutputExpenses(filteredYear);
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={changeExpensesFilterHandler} />
-                {props.expenseData.map((expense) => (
+                {outputExpenses.map((expense) => (
                     <ExpenseItem
                         key={expense.id} // Using key to let React identified which item is changed or add
                         date={expense.date}
